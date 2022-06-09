474def9881446d5d31f8c7c99490d87d59187675
VanHauTran2001 <86037948+VanHauTran2001@users.noreply.github.com>
2022-06-09 20:42:11 +0700

    update new

--- app/src/main/java/com/example/appmp3online/Login/Signin/ISigninView.java
+++
@@ -1,6 +0,0 @@
-package com.example.appmp3online.Login.Signin;
-
-public interface ISigninView {
-    void customLogin();
-    void onChecked();
-}
--- app/src/main/java/com/example/appmp3online/Login/Signup/ISignupView.java
+++
@@ -1,5 +0,0 @@
-package com.example.appmp3online.Login.Signup;
-
-public interface ISignupView {
-    void onClickSignup();
-}
